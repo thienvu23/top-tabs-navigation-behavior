18c7f4655dd61bdfe96909c807bcdd8d1ece6c08
Thien Bun <thienvq@zigvy.com>
2024-05-22 14:07:50 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { useRef, useState } from 'react';
+import { Button, LogBox, StyleSheet, Text, View } from 'react-native';
 
+LogBox.ignoreAllLogs();
+
+const initTabs = ['Screen_1', 'Screen_2', 'Screen_3', 'Screen_4'];
 export default function App() {
+  const [tabs, setTabs] = useState(initTabs)
+
+  const Tab = useRef(createMaterialTopTabNavigator()).current;
+
+  const Component = ({it}: {it: string}) => {
+    return <View style={{gap: 10, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text style={{fontSize: 30, fontWeight: 'bold'}}>
+      {it}
+    </Text>
+    <Button title='Remove This Tab' onPress={() => {
+        setTabs((preTabs) => {
+          return preTabs.filter(name => name != it);
+        })
+    }}/>
+    <Button title=' Reset Tabs' onPress={() => setTabs(initTabs)} />
+  </View>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{paddingTop: 60, flex: 1}}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          {tabs.map(it => <Tab.Screen key={it} name={it} component={() => <Component it={it} />} />)}
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
